4cd3d5cc335db4bff587ac51ed3c3eab69af90a4
pnicolaum <p@nicolau.com>
2025-04-29 17:07:42 +0200

    init backend and connecting to mongo db

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+
+const router = express.Router();
+
+router.get("/register", async (req, res) => {
+    res.send("register");
+})
+
+router.get("/login", async (req, res) => {
+    res.send("login");
+})
+
+export default router;
