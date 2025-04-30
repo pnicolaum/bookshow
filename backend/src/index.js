4edcabcf60a17764b519d23f9697e9231a74a97e
pnicolaum <p@nicolau.com>
2025-04-30 14:05:19 +0200

    book api and middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 // import job from "./lib/cron.js";
 
 import authRoutes from "./routes/authRoutes.js";
-//import bookRoutes from "./routes/bookRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
-//app.use("/api/books", bookRoutes);
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
