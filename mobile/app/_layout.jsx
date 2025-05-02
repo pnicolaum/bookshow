27cc8e763d425912dddd046f497d64292ef3a59f
pnicolaum <p@nicolau.com>
2025-05-02 16:42:47 +0200

    profile and last changes

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,9 +1,11 @@
-import { SplashScreen, Stack, useRouter, useSegments  } from "expo-router";
+import { SplashScreen, Stack, useRouter, useSegments } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import { useFonts } from "expo-font";
+
 import { useAuthStore } from "../store/authStore";
+import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -12,6 +14,14 @@ export default function RootLayout() {
   const segments = useSegments();
 
   const { checkAuth, user, token } = useAuthStore();
+
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded]);
 
   useEffect(() => {
     checkAuth();
@@ -27,14 +37,14 @@ export default function RootLayout() {
   }, [user, token, segments]);
 
   return (
-  <SafeAreaProvider>
-    <SafeScreen>
-      <Stack screenOptions={{ headerShown: false }} >
-        <Stack.Screen name="(tabs)"/>
-        <Stack.Screen name="(auth)"/>
-      </Stack>
-    </SafeScreen>
-    <StatusBar style="dark" />
-  </SafeAreaProvider>
-  )
+    <SafeAreaProvider>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </SafeScreen>
+      <StatusBar style="dark" />
+    </SafeAreaProvider>
+  );
 }
